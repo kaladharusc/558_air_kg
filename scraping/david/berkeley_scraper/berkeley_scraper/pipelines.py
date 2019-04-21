5c4d2c3464cc0c916213826256dc27c1ea8e01de
David Goodfellow <dgoodfel@usc.edu>
2019-04-21 12:09:01 -0700

    Update pipelines.py

--- scraping/david/berkeley_scraper/berkeley_scraper/pipelines.py
+++ scraping/david/berkeley_scraper/berkeley_scraper/pipelines.py
@@ -7,5 +7,18 @@
 
 
 class BerkeleyScraperPipeline(object):
+    def open_spider(self, spider):
+        self.file = open("{}/berkeley_scraper/data/berkeley_course_explorer_data.json".\
+            format(os.getcwd()), "w")
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
+
+
+    # def process_item(self, item, spider):
+        # return item
